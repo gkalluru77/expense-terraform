aa49ae0bc89c81440e59b86dcfe5fa2fbb5cca35
gkalluru77 <girinath.kalluru@gmail.com>
2023-12-06 19:01:31 +0530

    terraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,3 +2,12 @@ resource "aws_vpc" "main" {
   cidr_block        = var.vpc_cidr_block
   tags              = merge(var.tags, { Name = var.env })
 }
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  tags              = merge(var.tags, { Name = "public-subnet" })
+  availability_zone = var.azs[count.index]
+}
+
