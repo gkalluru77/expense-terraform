0a34669d0ad0f49c8cbb56599c67fbbb77efb93d
girin <girinath.kalluru@gmail.com>
2024-04-08 11:52:27 +0530

    sample

--- modules/app/userdata.tf
+++ modules/app/userdata.tf
@@ -2,4 +2,6 @@
 
 #yum install ansible python3.11-pip.noarch -y | tee -a /opt/userdata.log
 #pip3.11 install botocore boto3 | tee -a /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/gkalluru77/expense-terraform.git expense.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
+#ansible-pull -i localhost, -U https://github.com/gkalluru77/expense-terraform.git expense.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
+
+ansible-pull -i localhost, -U https://github.com/gkalluru77/expense-ansible.git expense.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
