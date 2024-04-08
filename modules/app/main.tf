a4ad733be3d95fa9eb1ce0c1da9a2c0b7ec59e45
girin <girinath.kalluru@gmail.com>
2024-04-08 19:04:51 +0530

    sample

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "main" {
 
 
 resource "aws_launch_template" "main" {
-  name_prefix   = "${var.env}-${var.component}"
+  name          = "${var.env}-${var.component}"
   image_id      = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
@@ -42,6 +42,10 @@ resource "aws_launch_template" "main" {
     role_name = var.component
     env       = var.env
   }))
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.main.name
+  }
 
 }
 
@@ -62,8 +66,67 @@ resource "aws_autoscaling_group" "main" {
       propagate_at_launch = true
 
   }
+}
+
+resource "aws_iam_role" "main" {
+  name = "${var.env}-${var.component}"
+  tags = merge(var.tags, { Name = "${var.env}-${var.component}" })
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "SSM-Read-Access"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "GetResources",
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : [
+            "arn:aws:ssm:us-east-1:878283146072:parameter/${var.env}.${var.component}.*",
+           /* "arn:aws:ssm:us-east-1:878283146072:parameter/newrelic.licence_key",
+            "arn:aws:ssm:us-east-1:878283146072:parameter/${var.env}.rds.*",
+            "arn:aws:ssm:us-east-1:878283146072:parameter/grafana.api_key",
+            "arn:aws:ssm:us-east-1:878283146072:parameter/jenkins.*", */
+          ]
+        },
+          {
+          "Sid" : "ListResources",
+          "Effect" : "Allow",
+          "Action" : "ssm:DescribeParameters",
+          "Resource" : "*"
+        },
+      ]
+    })
+  }
 
 }
+resource "aws_iam_instance_profile" "main" {
+  name = "${var.env}-${var.component}"
+  role = aws_iam_role.main.name
+}
+
+
+
 
 
 
